647b130b8250b317719b1ddd0969872e4294012f
Neil Henderson <ne.il@hotmail.co.uk>
2016-07-24 21:01:52 +0100

    250 test games

--- public/js/actions.js
+++ public/js/actions.js
@@ -2,17 +2,6 @@ angular.module('Game').factory('actions', function ($rootScope) {
   $rootScope.game = state;
   $rootScope.meta = {you: 0};
 
-  $rootScope.control = {
-    'person': {
-      if: function(game) {
-        return true;
-      },
-      click: function(game) {
-        game.finished = true;
-      }
-    } 
-  }
-
   actions.applyMove = function(move, game) {
   	var newState = actions[move.kind](move, game, game.players[game.currentPlayer]);
 
