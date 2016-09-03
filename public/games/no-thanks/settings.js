6e70931ae3d1927752e48a53f64518edf62b7066
Neil Henderson <ne.il@hotmail.co.uk>
2016-09-03 13:17:38 +0100

    no thanks settings

---
+++ public/games/no-thanks/settings.js
@@ -0,0 +1,32 @@
+module.exports = {
+  n: 250,
+  c: Math.SQRT2,
+  broader: false,
+  bias: {
+    db: 0,
+    freq: 0,
+    seq: 0,
+    turn: 0,
+    user: 0
+  },
+  sim: {
+    freq: 0,
+    seq: 0,
+    turn: 0,
+    user: 0,
+    boltzmann: 1
+  },
+  fpu: {
+    freq: 0,
+    seq: 0,
+    turn: 0,
+    user: 0
+  },
+  amv: {
+    simLength: -1,
+    freq: 0,
+    seq: 0,
+    turn: 0,
+    user: 0
+  }
+}
