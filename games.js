82d46ebe54c671bcd97ee1879bd62c742d6f92ed
Neil Henderson <ne.il@hotmail.co.uk>
2016-09-05 12:08:48 +0100

    no thanks not first in games list

--- games.js
+++ games.js
@@ -1,17 +1,17 @@
 module.exports = {
-  'no-thanks': {
-    link: 'no-thanks',
-    name: 'No Thanks',
-    color: '#ca0',
-    active: '#DDA',
-    inactive: '#550'
-  },
   'glory-to-rome': {
     link: 'glory-to-rome',
     name: 'Glory to Rome',
     color: '#222',
     active: '#CCC',
     inactive: '#333'
+  },
+  'no-thanks': {
+    link: 'no-thanks',
+    name: 'No Thanks',
+    color: '#ca0',
+    active: '#DDA',
+    inactive: '#550'
   }/*,
   'skull': {
     link: 'skull',
